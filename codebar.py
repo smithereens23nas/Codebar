5acee2c12d45f34e82a634e362c3088bb6275a7c
smiethereens23nas <nicholas23smith@gmail.com>
2022-02-02 20:18:12 -0500

    Test case is working

--- codebar.py
+++ codebar.py
@@ -6,22 +6,57 @@ class Member:
         print(f"Hi, my name is {self.fullname}")
 
 class Student(Member):
-    def __init__(self, reason, fullname):
+    def __init__(self, fullname, reason):
         super().__init__(fullname)
         self.reason = reason
         
 class Instructor(Member):
-    def __init__(self, fullname, bio, skills, add_skill):
+    def __init__(self, fullname, bio):
         super().__init__(fullname)
         self.bio = bio
-        self.skills = skills
-        self.add_skill
+        self.skills = []
         
-class Workshops():
+    def add_skill(self, skill):
+        self.skills.append(skill)
+        
+class Workshop():
     def __init__(self, date, subject):
         self.date = date
         self.subject = subject
         self.student = []
         self.instructor = []
         
+    def add_participant(self, member):
+        self.member = member
+        if(type(member).__name__) == 'Student':
+            self.student.append(member)
+        elif(type(member).__name__) == "Instructor":
+            self.instructor.append(member)
+        else:
+            print('member does not exist')
+            
+    
+    def print_details(self):
+        print(f"Workshop details: {self.date}, {self.subject}")
+        self.print_students()
+        
+    def print_students(self):
+        for i in self.student:
+            print(vars(i['fullname']))
+    
+workshop = Workshop("12/03/2014", "Shutl")
+
+jane = Student("Jane Doe", "I am trying to learn programming and need some help")
+lena = Student("Lena Smith", "I am really excited about learning to program!")
+vicky = Instructor("Vicky Python", "I want to help people learn coding.")
+vicky.add_skill("HTML")
+vicky.add_skill("JavaScript")
+nicole = Instructor("Nicole McMillan", "I have been programming for 5 years in Python and want to spread the love")
+nicole.add_skill("Python")
+
+workshop.add_participant(jane)
+workshop.add_participant(lena)
+workshop.add_participant(vicky)
+workshop.add_participant(nicole)
+workshop.print_details()
     
